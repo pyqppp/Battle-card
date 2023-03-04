a1892814a476c6d3192a9db5496ccfbb3d948428
Red <pyqppp@126.com>
2023-03-04 19:36:12 +0800

    创建了几个简单的攻击武器变量

---
+++ Battle card.py
@@ -0,0 +1,7 @@
+# 导入库
+import random
+
+# 创建变量
+ak47 = {'attack': 50, 'name': 'ak47', 'kind': 'attack', 'step': 1.5}
+m416 = {'attack': 35, 'name': 'm416', 'kind': 'attack', 'step': 1.0}
+p18c = {'attack': 20, 'name': 'ak47', 'kind': 'attack', 'step': 0.5}
