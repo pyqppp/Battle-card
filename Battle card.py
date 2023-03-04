995f090a39095ebc77700bcfe910ad7a3154748e
Red <pyqppp@126.com>
2023-03-05 07:51:58 +0800

    创建了几个简单的防御、治疗变量

--- Battle card.py
+++ Battle card.py
@@ -2,6 +2,8 @@
 import random
 
 # 创建变量
-ak47 = {'attack': 50, 'name': 'ak47', 'kind': 'attack', 'step': 1.5}
-m416 = {'attack': 35, 'name': 'm416', 'kind': 'attack', 'step': 1.0}
-p18c = {'attack': 20, 'name': 'ak47', 'kind': 'attack', 'step': 0.5}
+ak47 = {'attack': 50, 'name': 'ak47', 'kind': 'attack', 'step': 1.5, 'repeat': True}
+m416 = {'attack': 35, 'name': 'm416', 'kind': 'attack', 'step': 1.0, 'repeat': True}
+p18c = {'attack': 20, 'name': 'p18c', 'kind': 'attack', 'step': 0.5, 'repeat': True}
+Riot_shield = {'defense': 30, 'name': '防爆盾', 'kind': 'defense', 'step': 0.5, 'repeat': False}
+bandage = {'treat': 30, 'name': '绷带', 'kind': 'treat', 'step': 0.5, 'repeat': False}
